562a5dfc803c27c08eb423c7bb6a0247e434dea4
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2025-11-09 21:33:22 +0200

    new config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
+  base: './',
   build: {
-    outDir: "build/client",
+    outDir: 'dist',
+    sourcemap: false,
+  },
+  server: {
+    port: 5173,
+    open: true,
   },
 });
